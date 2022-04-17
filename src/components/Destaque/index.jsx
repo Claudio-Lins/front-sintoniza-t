67e2a849aa0758ff7b87f62a9a148393f4323203
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-17 22:53:53 +0100

    .

--- src/components/Destaque/index.jsx
+++ src/components/Destaque/index.jsx
@@ -3,24 +3,16 @@ import ArticleCard from './ArticleCard'
 import { useWindowSize } from '../../../hooks/useWindowSize'
 import Link from 'next/link'
 
-interface DestqueProps {
-  destaque: {
-    id: number
-    slug: string
-    title: string
-    content: string
-  }
-  windowWidth: number
-}
 
-export function Destaque({destaque}:DestqueProps) {
+
+export function Destaque({destaque}) {
   const { windowWidth } = useWindowSize()
   return (
     <div>
       <div className='flex items-center justify-between gap-6 mt-10'>
           {destaque
             .slice(0, windowWidth < 768 ? 1 : windowWidth < 1280 ? 2 : 3)
-            .map((destaque: { id: React.Key | null | undefined; slug: any }) => (
+            .map((destaque) => (
               <div key={destaque.id} className=''>
                 <Link href={`/articles/${destaque.slug}`}>
                   <a className=''>
