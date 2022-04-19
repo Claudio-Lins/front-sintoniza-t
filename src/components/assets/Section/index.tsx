8f5211fd81370ab04e845b1e1d1bd7bda0c5f182
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-19 20:13:28 +0100

    Update index.tsx

--- src/components/assets/Section/index.tsx
+++ src/components/assets/Section/index.tsx
@@ -13,7 +13,7 @@ export function Section({ sectioColor, children, id }: SectioProps) {
     <div
       id={id}
       className={`flex min-h-[50vh] flex-col items-center px-4 pt-10 pb-20 shadow-xl
-      ${sectioColor ? 'bg-green-50' : 'bg-white'}
+      ${sectioColor ? 'bg-gradient-to-b from-green-200 to-green-50 shadow-xl shadow-green-50' : 'bg-white'}
   `}
     >
       <div className="flex w-full max-w-7xl 2xl:max-w-[1480px] flex-col items-center justify-center">
