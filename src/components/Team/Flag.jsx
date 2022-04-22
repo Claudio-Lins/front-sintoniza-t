11ba1940a552b34f3921bc422c366842ce2e1156
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-04-22 19:39:37 +0100

    flag country

--- src/components/Team/Flag.jsx
+++ src/components/Team/Flag.jsx
@@ -1,17 +1,16 @@
 import ReactCountryFlag from 'react-country-flag'
 
-
-export function Flag({ nationality }) {
-    return (
-        <ReactCountryFlag
-            countryCode={nationality}
-            // countryCode={nationality?.slice(0, 2)}
-            svg
-            style={{
-              width: '3rem',
-              height: '3rem',
-              objectFit: 'cover',
-            }}
-          />
-    )
+export function Flag({ country }) {
+  return (
+    <ReactCountryFlag
+      countryCode={country}
+      // countryCode={country?.slice(0, 2)}
+      svg
+      style={{
+        width: '3rem',
+        height: '3rem',
+        objectFit: 'cover',
+      }}
+    />
+  )
 }
