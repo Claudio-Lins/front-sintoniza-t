ce563961783f477b595a730f0d30dd4df3369d2d
Claudio Lins <claudio.lins@me.com>
2022-06-09 15:55:28 +0100

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,34 +1,36 @@
-import NextAuth from "next-auth";
-import CredentialProvider from "next-auth/providers/credentials";
+import NextAuth from 'next-auth'
+import CredentialProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
   providers: [
     CredentialProvider({
-      name: "credentials",
-      type: "credentials",
+      name: 'credentials',
+      type: 'credentials',
       credentials: {
         username: {
-          label: "Email",
-          type: "text",
-          placeholder: "johndoe@test.com",
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email address',
         },
-        password: { label: "Password", type: "password" },
+        password: { label: 'Password', type: 'password' },
       },
       authorize: (credentials) => {
         // database look up
         if (
-          credentials.username === "john" &&
-          credentials.password === "test"
+          credentials.username === 'sintoniza-t@gmail.com' &&
+          credentials.password === '123456'
         ) {
           return {
             id: 2,
-            name: "John",
-            email: "johndoe@test.com",
-          };
+            name: 'Sintoniza-t Admin',
+            email: 'sintoniza-t@gmail.com',
+            image:
+              'https://admin-sintoniza-t.vercel.app/_next/image?url=%2Flogo%2FlogoIcon.png&w=3840&q=75',
+          }
         }
 
         // login failed
-        return null;
+        return null
       },
     }),
   ],
@@ -36,25 +38,25 @@ export default NextAuth({
     jwt: ({ token, user }) => {
       // first time jwt callback is run, user object is available
       if (user) {
-        token.id = user.id;
+        token.id = user.id
       }
 
-      return token;
+      return token
     },
     session: ({ session, token }) => {
       if (token) {
-        session.id = token.id;
+        session.id = token.id
       }
 
-      return session;
+      return session
     },
   },
-  secret: "test",
+  secret: 'test',
   jwt: {
-    secret: "test",
+    secret: 'test',
     encryption: true,
   },
   // pages: {
   //   signIn: "/login",
   // },
-});
+})
