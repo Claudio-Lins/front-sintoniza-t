04c3e6586b03c0cc64fc06086b74d225e33c134f
Claudio Lins <claudio.lins@me.com>
2022-06-11 23:30:14 +0100

    imprensa

--- src/pages/admin/imprensa.jsx
+++ src/pages/admin/imprensa.jsx
@@ -1,20 +1,17 @@
-import { useSession, signIn, signOut, getSession } from 'next-auth/react'
-import { getAllImprensa } from "../api/sintonizat-api/imprensa/getAllImprensa"
-
-
-
+import { getSession } from 'next-auth/react'
+import { getAllImprensa } from '../api/sintonizat-api/imprensa/getAllImprensa'
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context);
+  const session = await getSession(context)
   const imprensa = await getAllImprensa()
 
   if (!session) {
     return {
       redirect: {
-        destination: "/api/auth/signin",
+        destination: '/api/auth/signin',
         permanent: false,
       },
-    };
+    }
   }
 
   return {
@@ -22,16 +19,50 @@ export async function getServerSideProps(context) {
       session,
       imprensa: JSON.parse(JSON.stringify(imprensa)),
     },
-  };
+  }
 }
 
-export default function Imprensa({imprensa}) {
+export default function Imprensa({ imprensa }) {
   return (
-    <div>
-      <pre>
-        {JSON.stringify(imprensa, null, 2)}
-      </pre>
+    <div className="flex flex-col px-4">
+      <div className="mt-8 rounded bg-white p-4 shadow 2xl:p-8">
+        <div className="p-4">
+          <h2 className="rounded-t-xl bg-gradient-to-r from-purple-800 to-green-800 p-2 text-center text-2xl font-semibold text-white">
+            Subscribers - Newsletter
+          </h2>
+          <table className="mt-2 w-full">
+            <thead>
+              <tr className="w-full rounded bg-green-300 text-green-900">
+                <th className="p-4 text-left text-xs sm:text-lg">Nome</th>
+                <th className="p-4 text-left text-xs sm:text-lg">Email</th>
+                <th className="p-4 text-center text-xs sm:text-lg">Ações</th>
+                <th className="p-4 text-center text-xs sm:text-lg">Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              {imprensa.map((imprensa, i) => (
+                <tr
+                  key={i}
+                  className={`${i % 2 === 0 ? 'bg-white' : 'bg-green-100'} `}
+                >
+                  <td className="p-4 text-left text-xs font-semibold sm:text-base">
+                    {imprensa.title}
+                  </td>
+                  <td className="p-4 text-left text-xs sm:text-base">
+                    {imprensa.linkYoutube}
+                  </td>
+                  <td className="flex justify-center p-4 text-xs sm:text-base text-green-900 hover:text-red-700">
+                    {/* <button onClick={() => deleteNewsletter(newsletter.id)}>{IconTrash}</button> */}
+                  </td>
+                  <td className="p-4 text-center text-xs sm:text-base">
+                    {imprensa.datePublished}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
-
--- src/pages/api/sintonizat-api/imprensa/getAllImprensa.ts
+++ src/pages/api/sintonizat-api/imprensa/getAllImprensa.ts
@@ -2,11 +2,18 @@ import prisma  from '../../../../../lib/prisma'
 
 export async function getAllImprensa() {
   const data = await prisma.imprensa.findMany({
+    select: {
+      id: true,
+      title: true,
+      datePublished: true,
+    },
+
     orderBy: [
       {
         datePublished: 'desc'
-      }
-    ]
+      },
+    ],
+
   })
   return data
 }
