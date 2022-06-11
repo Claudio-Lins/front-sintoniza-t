21d5638e63e2fd2abee766d4b98810580c7debd1
Claudio Lins <claudio.lins@me.com>
2022-06-11 14:19:45 +0100

    Newsletter orderBy createAt: desc

--- src/pages/api/sintonizat-api/newsletter/getAllNewsletters.ts
+++ src/pages/api/sintonizat-api/newsletter/getAllNewsletters.ts
@@ -1,6 +1,12 @@
 import prisma  from '../../../../../lib/prisma'
 
 export async function getAllNewsletters() {
-  const data = await prisma.newsletter.findMany()
+  const data = await prisma.newsletter.findMany({
+    orderBy: [
+      {
+        createdAt: 'desc'
+      }
+    ]
+  })
   return data
 }
