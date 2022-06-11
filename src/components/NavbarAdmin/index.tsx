6bb24f5d1a496f3fc0bacc080ed183f9a9ea56c9
Claudio Lins <claudio.lins@me.com>
2022-06-11 19:49:06 +0100

    Update index.tsx

--- src/components/NavbarAdmin/index.tsx
+++ src/components/NavbarAdmin/index.tsx
@@ -5,8 +5,10 @@ import styles from './menu.module.css'
 import LogoH from '../../../public/logo/sintoniza-t_hor.png'
 import { navigationLinksAdmin } from '../../../utils/navigationLinksAdmin'
 import { useRouter } from 'next/router'
+import { signOut, useSession } from 'next-auth/react'
 
 export function NavbarAdmin() {
+  const { data: session } = useSession()
   const router = useRouter()
 
   return (
@@ -27,23 +29,37 @@ export function NavbarAdmin() {
             </a>
           </Link>
         </div>
-        {/* /Logo */}
-        {/* Menu */}
         <nav className="flex h-[100%] flex-1 items-center justify-center gap-6">
           {navigationLinksAdmin.map((link, i) => (
-            <div className="flex justify-center items-center gap-1">
+            <div className="flex items-center justify-center gap-1" key={i}>
               <div>{link.icon}</div>
-              <Link href={link.path} passHref key={i}>
-                <a
-                  className={`flex h-full items-center justify-center text-sm font-light uppercase text-green-900
-              `}
-                >
+              <Link href={link.path} passHref>
+                <a className="flex h-full items-center justify-center text-sm font-light uppercase text-green-900">
                   {link.label}
                 </a>
               </Link>
             </div>
           ))}
         </nav>
+        <div className="flex flex-col items-center justify-center">
+          <div className="">
+            <Image
+              src={session.user.image}
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </div>
+          <p className="text-xs font-bold text-green-800">
+            {session.user.name}
+          </p>
+          <button
+            onClick={() => signOut()}
+            className="text-xs font-bold text-red-600 hover:text-red-800"
+          >
+            Sair
+          </button>
+        </div>
       </div>
     </header>
   )
