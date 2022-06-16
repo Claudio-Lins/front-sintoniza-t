a6e445c2b085d3b807dc49af338239fe36d1a592
Claudio Lins <claudio.lins@me.com>
2022-06-16 16:02:06 +0100

    newsletter & imprensa ok local

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
+import { AppProps } from 'next/app'
+import { ToastProvider } from '../../hooks/useToast'
 import { WindowSizeProvider } from '../../hooks/useWindowSize'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component,  session, ...pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <WindowSizeProvider>
+      <ToastProvider>
         <SessionProvider session={session}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
         </SessionProvider>
+      </ToastProvider>
     </WindowSizeProvider>
   )
 }
