a6064e71531fd29a837f04c25acb52062387c014
Claudio Lins <claudio.lins@me.com>
2022-06-19 19:09:07 +0100

    Update newsletter.jsx

--- src/pages/admin/newsletter.jsx
+++ src/pages/admin/newsletter.jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { IconTrash } from '../../components/icons'
 import { getAllNewsletters } from '../api/sintonizat-api/newsletter/getAllNewsletters'
 import { HeaderContent } from '../../components/template/HeaderContent'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function Newsletter({ newsletter }) {
   const router = useRouter()
@@ -26,12 +27,36 @@ export default function Newsletter({ newsletter }) {
   }
 
   return (
-    <div className="flex flex-col px-4">
+    <div className="flex flex-col">
       <HeaderContent
-        title={'Newsletter'}
-        subtitle="Contactos dos inscritos para receber a newsletter"
+        title={'Imprensa'}
+        subtitle="Materias relacionadas com a Sitoniza-t"
       />
-      <table className="w-full overflow-hidden rounded-xl">
+      <Toaster />
+      <div className="flex w-full flex-wrap justify-center gap-2 sm:px-4">
+        {newsletter?.map((newsletter, i) => {
+          return (
+            <div
+              key={newsletter.id}
+              className="relative mb-2 w-full max-w-md rounded-lg border bg-teal-100 p-2 shadow-md sm:p-4"
+            >
+              <button
+                onClick={() => deleteNewsletter(newsletter.id)}
+                className="absolute right-2 top-2 flex h-6 w-6 items-center justify-center rounded-full bg-gray-50 p-1 font-bold shadow-sm hover:bg-teal-800 hover:text-white hover:shadow-inner"
+              >
+                x
+              </button>
+              <div className="flex flex-col">
+                <div className="text-[10px] sm:text-sm">Nome</div>
+                <div className="font-bold sm:text-xl">{newsletter.name}</div>
+                <div className="text-[10px] sm:text-sm">Email</div>
+                <div className="font-bold">{newsletter.email}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      {/* <table className="overflow-hidden rounded-xl">
         <thead className="bg-gradient-to-r from-teal-700 to-teal-500 text-sm font-bold tracking-wider text-white">
           <tr className=" text-xl">
             <th className="p-4 text-left">Nome</th>
@@ -61,7 +86,7 @@ export default function Newsletter({ newsletter }) {
             )
           })}
         </tbody>
-      </table>
+      </table> */}
     </div>
   )
 }
